27da822786afee51351369fdc0b8005fdd23b960
Himanshujain12345 <134693606+Himanshujain12345@users.noreply.github.com>
2023-06-30 14:26:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let inputBox = document.getElementById("inputbox");
+let addBtn = document.getElementById("addBtn");
+let listContainer = document.getElementById("listContainer");
+
+listContainer.innerHTML = localStorage.getItem("todo-list");
+
+function addTask() {
+    if (inputBox.value == '') {
+        alert('Invalid text :(\nPlease write something to ADD!');
+        return;
+    }
+
+
+let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("unchecked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+function saveData() {
+    localStorage.setItem("todo-list", listContainer.innerHTML);
+}
